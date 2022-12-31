cf4d0e4eef4be9601fee317f6aa2cd807676d667
tasoulis <kaputtebriefen@gmail.com>
2022-12-31 14:44:07 +0200

    added multiplication support

--- calc.c
+++ calc.c
@@ -4,25 +4,39 @@
 
 int main(int argc , char *argv[]){
 
-    int i,y,x;
+    int i,y,x,p;
     int equalA;
     int equalAdd;
+    int equalM;
+    int equalMulitply;
     int sum=0;
     for(i=1; i<=argc-1; i++)
     {
+        equalM= strcmp("-m",argv[i]);
+        equalMulitply= strcmp("--Multiply",argv[i]);
         equalA= strcmp("-a",argv[i]);
         equalAdd= strcmp("--add",argv[i]);
-        if (equalA==0 || equalAdd==0)
+        if (equalA==0 || equalAdd==0|| equalM==0 || equalMulitply==0)
         {
             y= atoi(argv[i+1]);
             x= atoi(argv[i+2]);
-            sum=x+y;
-            printf("num1 + num2 = %d\n",sum);
+            if(equalM==0 || equalMulitply==0)
+            {
+                p=x*y;
+                printf("num1 * num2 = %d\n",p);
+            }
+            else
+            {
+                sum=x+y;
+                printf("num1 + num2 = %d\n",sum);
+            }
+            
+            
             break;
         }
         else
         {
-            printf("correct syntax is calc -a <num1,num2>\n");
+            printf("correct syntax is: calc <option> <num1,num2>\n");
             break;
         }
        
