ac6077f84d80afa2cb7b21cb6abea051d320a2a4
tasoulis <kaputtebriefen@gmail.com>
2023-01-07 22:57:54 +0200

    replaced atoi function with custom code

--- calc.c
+++ calc.c
@@ -4,7 +4,7 @@
 
 int main(int argc , char *argv[]){
 
-    int i,y,x,p,new_digit,current_number=0;
+    int i,y,x,p,k,new_digit,current_number=0;
     int equalA;
     int equalAdd;
     int equalM;
@@ -12,34 +12,43 @@ int main(int argc , char *argv[]){
     int sum=0;
     char ch;
 
-    for(i=1; i<=argc-1; i++)
+    i=1;
+    while(i<=argc-1)
     {
+        
         equalM= strcmp("-m",argv[i]);
-        equalMulitply= strcmp("--Multiply",argv[i]);
+        equalMulitply= strcmp("--multiply",argv[i]);
         equalA= strcmp("-a",argv[i]);
         equalAdd= strcmp("--add",argv[i]);
         if (equalA==0 || equalAdd==0|| equalM==0 || equalMulitply==0)
         {
-            ch=*argv[i+1];
+            //get first number
+            k=1;
             current_number=0;
+            ch=*argv[i+1];
             while(ch!='\0')
             {   
                 new_digit= ch - '0' ; // convert digit to integer
                 current_number = (current_number*10) + new_digit;
-                ch= *(argv[i+1] +1);
+                ch= *(argv[i+1] +k);
+                k+=1;
             }
-            y=  current_number;
+            y = current_number;
+            
+            //get second number
+            k=1;
             current_number=0;
             ch=*argv[i+2];
             while(ch!='\0')
             {   
                 new_digit= ch - '0' ; // convert digit to integer
                 current_number = (current_number*10) + new_digit;
-                ch= *(argv[i+2] +1);
+                ch= *(argv[i+2] +k);
+                k+=1;
             }
             x=  current_number;
 
-
+            //calculate 
             if(equalM==0 || equalMulitply==0)
             {
                 p=y*x;
@@ -59,7 +68,7 @@ int main(int argc , char *argv[]){
                 printf("correct syntax is: calc <option> <num1,num2>\n");
                 break;
         }
-       
+       i++;
     }
     
 
