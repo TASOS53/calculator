706e83593d875d93c6da7eb28c135f7116d3200b
tasoulis <kaputtebriefen@gmail.com>
2023-01-13 18:25:10 +0200

    Replaced code with functions ,plus added support for negative numbers

--- calc.c
+++ calc.c
@@ -2,20 +2,27 @@
 #include<string.h>
 #include<stdlib.h>
 
+
+
+
+int my_atoi(char *s);
+int add(int , int);
+int multiply(int , int);
+
 int main(int argc , char *argv[]){
 
-    int i,y,x,p,k,new_digit,current_number=0;
+    int i,y,x;
     int equalA;
     int equalAdd;
     int equalM;
     int equalMulitply;
-    int sum=0;
-    char ch;
+    int addition_result;
+    int multiplication_result;
+    
 
     i=1;
     while(i<=argc-1)
     {
-        
         equalM= strcmp("-m",argv[i]);
         equalMulitply= strcmp("--multiply",argv[i]);
         equalA= strcmp("-a",argv[i]);
@@ -23,55 +30,72 @@ int main(int argc , char *argv[]){
         if (equalA==0 || equalAdd==0|| equalM==0 || equalMulitply==0)
         {
             //get first number
-            k=1;
-            current_number=0;
-            ch=*argv[i+1];
-            while(ch!='\0')
-            {   
-                new_digit= ch - '0' ; // convert digit to integer
-                current_number = (current_number*10) + new_digit;
-                ch= *(argv[i+1] +k);
-                k+=1;
-            }
-            y = current_number;
+            y = my_atoi(argv[i+1]);
             
             //get second number
-            k=1;
-            current_number=0;
-            ch=*argv[i+2];
-            while(ch!='\0')
-            {   
-                new_digit= ch - '0' ; // convert digit to integer
-                current_number = (current_number*10) + new_digit;
-                ch= *(argv[i+2] +k);
-                k+=1;
-            }
-            x=  current_number;
+            x= my_atoi(argv[i+2]);
 
             //calculate 
             if(equalM==0 || equalMulitply==0)
             {
-                p=y*x;
-                printf("%d * %d = %d\n",y,x,p);
+                multiplication_result= multiply(x,y);
+                printf("%d * %d = %d\n",y,x,multiplication_result);
             }
             else
             {
-                sum=y+x;
-                printf("%d + %d = %d\n",y,x,sum);
+                addition_result= add(x,y);
+                printf("%d + %d = %d\n",y,x,addition_result);
             }
             
-           i+=2; //skip next two arguments when user request addition or multiplication 
+            i+=2; //skip next two arguments when user request addition or multiplication 
             
         }
         else
         {
-                printf("correct syntax is: calc <option> <num1,num2>\n");
-                break;
+            printf("correct syntax is: calc <option> <num1,num2>\n");
+            break;
         }
        i++;
     }
-    
-
-    
     return 0;
+}
+
+
+int my_atoi(char *s)
+{
+    int new_digit,current_number=0,k;
+    char ch ;
+    ch=*s;
+    k=1;
+    int sign=1;
+    if(ch=='-')
+    {
+        sign=sign*(-1);
+        ch=*(s+1);
+        k++;
+    }
+    while(ch!='\0')
+    {   
+        new_digit= ch - '0' ; // convert digit to integer
+        current_number = (current_number*10) + new_digit;
+        ch= *(s+k);
+        k+=1;
+    }
+    return ((current_number)*(sign));
+
+}
+
+int add(int y , int x)
+{ 
+    int sum;
+    sum=y+x;
+    return sum;
+    
+}
+
+int multiply(int x , int y)
+{
+    int p;
+    p=x*y;
+    return p;
 }
