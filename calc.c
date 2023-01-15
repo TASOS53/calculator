420825fbc1cd53aaf5de7e2c9e9d24bbf23aa2d6
tasoulis <kaputtebriefen@gmail.com>
2023-01-15 12:17:48 +0200

    Added support for decimal numbers

--- calc.c
+++ calc.c
@@ -5,19 +5,20 @@
 
 
 
-int my_atoi(char *s);
-int add(int , int);
-int multiply(int , int);
+float my_atoi(char *s);
+float add(float , float);
+float multiply(float , float);
 
 int main(int argc , char *argv[]){
 
-    int i,y,x;
+    int i;
+    float y,x;
     int equalA;
     int equalAdd;
     int equalM;
     int equalMulitply;
-    int addition_result;
-    int multiplication_result;
+    float addition_result;
+    float multiplication_result;
     
 
     i=1;
@@ -39,12 +40,12 @@ int main(int argc , char *argv[]){
             if(equalM==0 || equalMulitply==0)
             {
                 multiplication_result= multiply(x,y);
-                printf("%d * %d = %d\n",y,x,multiplication_result);
+                printf("%f * %f = %f\n",y,x,multiplication_result);
             }
             else
             {
                 addition_result= add(x,y);
-                printf("%d + %d = %d\n",y,x,addition_result);
+                printf("%f + %f = %f\n",y,x,addition_result);
             }
             
             i+=2; //skip next two arguments when user request addition or multiplication 
@@ -61,41 +62,68 @@ int main(int argc , char *argv[]){
 }
 
 
-int my_atoi(char *s)
+float my_atoi(char *s)
 {
-    int new_digit,current_number=0,k;
+    float FinalNumber=0;
+    float new_digit,current_number=0;
+    int k;
+    float Fcurrent_number=0;
     char ch ;
+    int sign=1;
+    float decimalConvert=0.1;
     ch=*s;
     k=1;
-    int sign=1;
     if(ch=='-')
     {
         sign=sign*(-1);
         ch=*(s+1);
         k++;
     }
-    while(ch!='\0')
+    while(ch!='\0' && ch!=',')
     {   
         new_digit= ch - '0' ; // convert digit to integer
         current_number = (current_number*10) + new_digit;
         ch= *(s+k);
         k+=1;
-    }
-    return ((current_number)*(sign));
+        
+
+    }  
+
+        if(ch==',')
+        {
+            new_digit=0;
+            
+            ch=*(s+k);
+            while(ch!='\0')
+            {
+                new_digit=ch-'0'; 
+                Fcurrent_number=Fcurrent_number + new_digit*(decimalConvert);
+                decimalConvert=decimalConvert*(0.1);
+                k++;
+                ch= *(s+k);
+                
+
+            }
+           
+           
+        }
+   
+    FinalNumber= current_number + Fcurrent_number;
+    return (( FinalNumber)*(sign));
 
 }
 
-int add(int y , int x)
+float add(float y , float x)
 { 
-    int sum;
+    float sum;
     sum=y+x;
     return sum;
     
 }
 
-int multiply(int x , int y)
+float multiply(float x , float y)
 {
-    int p;
+    float p;
     p=x*y;
     return p;
 }
